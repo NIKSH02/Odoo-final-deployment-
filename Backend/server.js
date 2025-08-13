85fbf1ba692fd90c71fd6ff24e2c07b6f3776c12
Nikhil Sharma <nikhilsharma0437@gmail.com>
2025-08-13 11:10:53 +0530

    port

--- Backend/server.js
+++ Backend/server.js
@@ -30,7 +30,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 connectDB();
 
